379d3a6fe9d06ed679e77f181d9499650db423c8
SDY8023 <18438609381@163.com>
2022-12-11 11:44:07 +0800

    hwjs

--- src/main/scala/HWJS/test5.scala
+++ src/main/scala/HWJS/test5.scala
@@ -6,47 +6,106 @@ import scala.collection.mutable.ListBuffer
 object test5 {
   def main(args: Array[String]): Unit = {
     val input = StdIn.readLine()
-    val priceAndPackge = input.split(" ")
-    val totalMoney = priceAndPackge(0).toInt
-    val count = priceAndPackge(1).toInt
-    // 创建物品集合(物品id,价格,满意度,是否为主属性)
-    val resultList: ListBuffer[(Int,Int, Int, Int)] = mutable.ListBuffer[(Int,Int, Int, Int)]()
-    val result:Int = 0
-    for(i <- 0 until(count)){
-      val data: String = StdIn.readLine()
-      val ds: Array[String] = data.split(" ")
-      resultList.append((i + 1,ds(0).toInt,ds(1).toInt,ds(2).toInt))
+    fun4(input)
+  }
+
+  def fun4(input:String): Unit ={
+    val totalMoneyAndData = input.split(" ")
+    val totalMoney:Int = totalMoneyAndData(0).toInt
+    val data:Int = totalMoneyAndData(1).toInt
+    // 记录每个产品的数据
+    val dataList: ListBuffer[(Int, Int, Int, Int)] = mutable.ListBuffer[(Int, Int, Int, Int)]()
+    for(i <- 0 until(data)){
+      val str: String = StdIn.readLine()
+      val strings1 = str.split(" ")
+      dataList.append((i + 1,strings1(0).toInt,strings1(1).toInt,strings1(2).toInt))
     }
-    // 统计每个主属性物品->附属物品
-    val resultMap = mutable.Map[Int,mutable.ListBuffer[(Int,Int, Int, Int)]]()
-    // 主属性物品统计进来
-    resultList.map(x => {
-      if(x._4 == 0) {
-        resultMap += (x._1 -> mutable.ListBuffer(x))
+    // 记录每个主属性id -> 附属物品
+    val dataMap = mutable.Map[Int,mutable.ListBuffer[(Int,Int,Int)]]()
+    dataList.foreach(x => {
+      if(x._4 == 0){
+        dataMap.put(x._1,ListBuffer((x._2,x._3,x._4)))
       }
     })
-    // 附属物品加入进来
-    resultList.map(x => {
-      if(x._4 != 0){
-        val tuples: ListBuffer[(Int, Int, Int, Int)] = resultMap.getOrElseUpdate(x._4, mutable.ListBuffer())
-        tuples.append(x)
-        resultMap.update(x._4,tuples)
+    dataList.foreach(x => {
+      if(dataMap.contains(x._4)){
+        val maybeTuples: ListBuffer[(Int, Int, Int)] = dataMap.get(x._4).get
+        maybeTuples.append((x._2,x._3,x._4))
+        dataMap.update(x._4,maybeTuples)
       }
     })
-    // 满意度集合
-    val ints: ListBuffer[Int] = mutable.ListBuffer[Int]()
-    var t:Int = 0
-    var m:Int = 0
-    resultMap.foreach(x => {
-      x._2.map(y => {
-        t += y._2 * y._3
-        m += y._2
-      })
-      if(m <= totalMoney){
-        ints.append(t)
+
+    var resultList: mutable.ListBuffer[(Int, Int)] = mutable.ListBuffer[(Int, Int)]()
+    var totalPrice:Int = 0
+    var totalIndex:Int = 0
+    dataMap.foreach(x => {
+      totalPrice = x._2(0)._1
+      totalIndex = x._2(0)._1 * x._2(0)._2
+      resultList.append((totalPrice,totalIndex))
+      var j:Int = 1
+      var i:Int = 1
+      var tmpPrice =  totalPrice
+      var tmpIndex = totalIndex
+      if(x._2.length != 1){
+        while(i < x._2.length){
+          totalPrice += x._2(i)._1
+          totalIndex += x._2(i)._1 * x._2(i)._2
+          resultList.append((totalPrice,totalIndex))
+          if(i == x._2.length -1){
+            tmpPrice += x._2(j)._1
+            tmpIndex += x._2(j)._1 * x._2(j)._2
+            totalPrice = tmpPrice
+            totalIndex = tmpIndex
+            j += 1
+            i = j
+          }else{
+            i += 1
+            totalPrice = tmpPrice
+            totalIndex = tmpIndex
+          }
+        }
       }
     })
-    print(ints.max)
+    var result:Int = 0
+    resultList = resultList.filter(_._1 <= totalMoney)
+
+    var a = 0
+    var b = 0
+    var tmpA = 0
+    var tmpB = 0
+    var j = 0
+    var i = 0
+    val rList = mutable.ListBuffer[Int]()
+    while(i < resultList.length){
+      rList.append(resultList(i)._2)
+      a = resultList(i)._1
+      b = resultList(i)._2
+      tmpA = a
+      tmpB = b
+      j = i + 1
+      var m = i
+      while(j < resultList.length){
+        a += resultList(j)._1
+        b += resultList(j)._2
+        if(a <= totalMoney){
+          rList.append(b)
+        }
+        j += 1
+        if(j == resultList.length - 1){
+          m += 1
+          j = m
+          tmpA += resultList(j)._1
+          tmpB += resultList(j)._2
+        }else{
+          a = tmpA
+          b = tmpB
+        }
+      }
+      i += 1
+
+    }
+    println(rList.max)
+
   }
 
 }
